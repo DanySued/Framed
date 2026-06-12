12c016aac2eefd6cc2034314ec07275ab781b4cf
Dany <danysued@gmail.com>
2026-06-12 18:52:50 +0300

    auto: sync

--- web/lib/fonts.ts
+++ web/lib/fonts.ts
@@ -1,15 +1,21 @@
-import { Fraunces, Inter } from "next/font/google";
+import { DM_Sans, Instrument_Serif, Geist_Mono } from "next/font/google";
 
-export const fraunces = Fraunces({
+export const dmSans = DM_Sans({
   subsets: ["latin"],
-  weight: "variable",
-  axes: ["opsz"],
+  weight: ["300", "400", "500", "600", "700"],
+  variable: "--font-sans",
+  display: "swap",
+});
+
+export const instrumentSerif = Instrument_Serif({
+  subsets: ["latin"],
+  weight: "400",
   variable: "--font-display",
   display: "swap",
 });
 
-export const inter = Inter({
+export const geistMono = Geist_Mono({
   subsets: ["latin"],
-  variable: "--font-sans",
+  variable: "--font-mono",
   display: "swap",
 });
